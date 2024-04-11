26467613bf46845b34fb51b6dbc86739e973d3d4
AnaPaulaa1987 <166718618+AnaPaulaa1987@users.noreply.github.com>
2024-04-11 11:13:11 -0300

    Add files via upload

---
+++ triangulo.py
@@ -0,0 +1,16 @@
+n1 = float(input(" digite o primeiro: "))
+n2 = float(input(" digite segundo tamanho "))
+n3 = float(input("digite o terceiro tamanho :"))
+
+if n1 ==n2 == n3:
+    print
+    ("ESTE É UM TRIÃNGULO equilátero.")
+
+elif n1 == n2 != n3:
+    print ("este é um triangulo isósceles. ")
+elif n1 != n2 != n3:    
+    print (" este é triãnguloescaleno" )
+
+else: 
+    print("não foi possível formar um triangulo")
+ 
