52e88c4af1c28a4a34edb83aa71785d9aa589ece
AnaPaulaa1987 <166718618+AnaPaulaa1987@users.noreply.github.com>
2024-04-11 10:36:40 -0300

    Add files via upload

---
+++ vverificador de  idade.py
@@ -0,0 +1,20 @@
+n1 = float(input(" digite sua idade:" ))
+if n1 >= 60:
+    print ("você é idoso ") 
+
+elif n1 >= 18:
+    print ("você é um adulto")
+elif n1>= 14 :
+    print ("você já é um adolescente")
+elif n1 <= 13:
+    print("você ainda é uma criança")
+else:
+    print ("erro de digitação , tente novamente")    
+
+
+
+
+
+
+
+
