3fc69215414e29a3f2e91e4d03cdc89b27ed74c6
AnaPaulaa1987 <166718618+AnaPaulaa1987@users.noreply.github.com>
2024-04-17 09:37:39 -0300

    Add files via upload

---
+++ calculadora de desconto.py
@@ -0,0 +1,5 @@
+print ("calculadora de desconto")
+n1 =  float(input ("digite o preço original: "))
+n2 = float(input (" digite a porcentagem de desconto: ")) 
+
+print (" o seu desconto é " , (n2 / 100 ) * n1 )
